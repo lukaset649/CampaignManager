2435c0916c068ef161f4562c4639f5b2073daecf
Lukasz <setlaklukasz@gmail.com>
2026-06-23 01:30:33 +0200

    add BalanceWidget

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,12 +1,17 @@
 import { Link, Outlet } from 'react-router-dom';
+import { BalanceWidget } from '../BalanceWidget/BalanceWidget';
+import './AppLayout.less';
 
 export const AppLayout = () => (
   <>
     <header className="app-header">
       <span className="app-header__title">Campaign Manager</span>
-      <Link to="/campaigns/new" className="app-header__add-link">
-        + Dodaj kampanię
-      </Link>
+      <div className="app-header__right">
+        <BalanceWidget />
+        <Link to="/campaigns/new" className="app-header__add-link">
+          + Dodaj kampanię
+        </Link>
+      </div>
     </header>
     <main className="app-main">
       <Outlet />
---
+++ src/components/BalanceWidget/BalanceWidget.tsx
@@ -0,0 +1,14 @@
+import { useCampaigns } from '../../hooks/useCampaigns';
+import './BalanceWidget.less';
+
+export const BalanceWidget = () => {
+  const { state } = useCampaigns();
+  const { balance } = state.billingAccount;
+
+  return (
+    <div className="balance-widget">
+      <span className="balance-widget__label">Stan konta</span>
+      <span className="balance-widget__amount">{balance.toFixed(2)} PLN</span>
+    </div>
+  );
+};
