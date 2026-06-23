acef1c148b716a777aab66be871d608962f2c9dd
Lukasz <setlaklukasz@gmail.com>
2026-06-23 20:28:41 +0200

    add TownCelect component

---
+++ src/components/TownSelect/TownSelect.tsx
@@ -0,0 +1,26 @@
+import { mockTowns } from '../../data/mockTowns';
+import './TownSelect.less';
+
+interface TownSelectProps {
+  value: string;
+  onChange: (value: string) => void;
+  error?: string;
+}
+
+export const TownSelect = ({ value, onChange, error }: TownSelectProps) => (
+  <div className="town-select">
+    <select
+      className={`town-select__select${error ? ' town-select__select--error' : ''}`}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      <option value="">-- wybierz miasto --</option>
+      {mockTowns.map((town) => (
+        <option key={town} value={town}>
+          {town}
+        </option>
+      ))}
+    </select>
+    {error && <span className="town-select__error">{error}</span>}
+  </div>
+);
