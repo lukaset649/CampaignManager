d7d18a63a72a300d3ac2aab83c4867777fedfcea
Lukasz <setlaklukasz@gmail.com>
2026-06-23 22:25:10 +0200

    Enhance KeywordList component with overflow handling and tooltip for hidden keywords

--- src/components/KeywordList/KeywordList.tsx
+++ src/components/KeywordList/KeywordList.tsx
@@ -2,14 +2,32 @@ import './KeywordList.less';
 
 interface KeywordListProps {
   keywords: string[];
+  maxVisible?: number;
 }
 
-export const KeywordList = ({ keywords }: KeywordListProps) => (
-  <div className="keyword-list">
-    {keywords.map((keyword) => (
-      <span key={keyword} className="keyword-list__tag">
-        {keyword}
-      </span>
-    ))}
-  </div>
-);
+export const KeywordList = ({ keywords, maxVisible = 6 }: KeywordListProps) => {
+  const visibleKeywords = keywords.slice(0, maxVisible);
+  const hiddenKeywords = keywords.slice(maxVisible);
+
+  return (
+    <div className="keyword-list">
+      {visibleKeywords.map((keyword) => (
+        <span key={keyword} className="keyword-list__tag">
+          {keyword}
+        </span>
+      ))}
+      {hiddenKeywords.length > 0 && (
+        <span className="keyword-list__overflow">
+          +{hiddenKeywords.length}
+          <span className="keyword-list__tooltip" role="tooltip">
+            {hiddenKeywords.map((keyword) => (
+              <span key={keyword} className="keyword-list__tooltip-tag">
+                {keyword}
+              </span>
+            ))}
+          </span>
+        </span>
+      )}
+    </div>
+  );
+};
