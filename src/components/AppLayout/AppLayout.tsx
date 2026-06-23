16ea0762add743cf1e5e963e6709eedaaa95535c
Lukasz <setlaklukasz@gmail.com>
2026-06-23 21:45:49 +0200

    Adjust app name display for mobile view

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -6,7 +6,10 @@ import './AppLayout.less';
 export const AppLayout = () => (
   <>
     <header className="app-header">
-      <span className="app-header__title">Campaign Manager</span>
+      <span className="app-header__title">
+        <span className="app-header__title-full">Campaign Manager</span>
+        <span className="app-header__title-short">CM</span>
+      </span>
       <div className="app-header__right">
         <BalanceWidget />
         <Link to={ROUTES.CAMPAIGN_NEW} className="app-header__add-link">
